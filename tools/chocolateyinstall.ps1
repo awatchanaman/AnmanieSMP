3f0e1940fab5dbf5dd101134f9108c3ee48ced6c
awatchanaman <94679765+awatchanaman@users.noreply.github.com>
2024-05-30 08:49:58 +0200

    correction version

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,11 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-
 $packageArgs = @{
   packageName    = 'anmaniesmp'
   softwareName   = 'anmaniesmp*'
   fileType       = 'exe'
-  url            = 'https://github.com/awatchanaman/AnmanieSMP/blob/87be284dd7929a88508a30918fc720aa5f600814/tools/AnmSMP24.exe'
+  url            = 'http://tiry73.free.fr/anm/AnmSMP24.exe'
   silentArgs     = '/VERYSILENT'
   validExitCodes = @(0)
   checksum       = 'F418B4FA4F228672DAB39B7557CF96F22102066ADB4E07C30AC90E21F1C67AF4'
@@ -20,7 +19,6 @@ Remove-Item -Path "$env:USERPROFILE\Desktop\AnmanieSMP.lnk" -Recurse -ErrorActio
 #Mise en place du Raccourci alluser sur le bureau
 $filepath = Join-Path "${env:programfiles(x86)}\AnmSMP" 'AnmanieSMP.exe'
 Install-ChocolateyShortcut -ShortcutFilePath "$env:PUBLIC\Desktop\AnmanieSMP.lnk" -TargetPath "$filepath"
-
 
 if ((Get-PackageParameters).NoDesktopShortcut) {
     Write-Host "(Get-PackageParameters).NoDesktopShortcut)"
